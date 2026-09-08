0f4b110f2303b7bef4baa57a9cfe271725278872
hanukolli <kollihanumanasvi112@gmail.com>
2026-09-08 12:10:10 -0400

    Update MonthlyFeature.tsx

--- components/MonthlyFeature.tsx
+++ components/MonthlyFeature.tsx
@@ -1,1 +1,29 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Mission from "@/components/Mission";
+import Impact from "@/components/Impact";
+import PackageContents from "@/components/PackageContents";
+import Safety from "@/components/Safety";
+import WhoWeServe from "@/components/WhoWeServe";
+import MonthlyFeature from "@/components/MonthlyFeature";
+import Team from "@/components/Team";
+import GetInvolved from "@/components/GetInvolved";
+import Footer from "@/components/Footer";
 
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <Mission />
+      <Impact />
+      <PackageContents />
+      <Safety />
+      <WhoWeServe />
+      <MonthlyFeature />
+      <Team />
+      <GetInvolved />
+      <Footer />
+    </main>
+  );
+}
